b6df26aedf0a99c283d443a36fe096f331fffe9d
hwigyeompark <phg5590@gmail.com>
2018-10-20 02:52:57 +0900

    first commit

---
+++ src/components/intro/Title.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Title extends Component{
+    render() {
+        const commonStyle = {
+          textAlign:"center",
+          fontWeight:"lighter"
+        };
+        const emphasizeName = {
+            fontSize: "1.2em"
+        };
+        return (
+            <div>
+                <h1 style={commonStyle}>"삶은 여행, 여행하는 개발자 <span style={emphasizeName}>'{this.props.name}'</span> 입니다."</h1>
+            </div>
+        );
+    };
+};
+export default Title;
