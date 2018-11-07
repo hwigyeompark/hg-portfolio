c718d11adc98fd665de907d294bb350b640a3ba8
hwigyeompark <phg5590@gmail.com>
2018-11-07 14:24:30 +0900

    complete Contact section

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,9 +3,17 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 class Button extends Component{
     render(){
+        const btnStyle = {
+                border : "1px solid #B4B3B3",
+                fontSize : "1.5rem",
+                padding: "15px",
+                margin : "1%",
+                borderRadius : "15px",
+                backgroundColor : "transparent"
+        };
         return(
             <div>
-                <button><FontAwesomeIcon icon={this.props.icon}/>{this.props.name}</button>
+                <button style={btnStyle}><FontAwesomeIcon icon={this.props.icon}/> {this.props.name}</button>
             </div>
         );
     }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,23 @@
 import React, {Component} from 'react';
 import {faGithub} from '@fortawesome/free-brands-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import '@fortawesome/react-fontawesome';
 import Button from "./Button";
 
 class Contact extends Component{
     render(){
         const boxStyle = {
             width:"60%",
-            margin:"0 auto",
+            margin: "0 auto",
             marginTop:"2%",
-            display:"flex"
+            display:"flex",
+            flexDirection : "column",
+            textAlign : "center",
+            justifyContent : "flex-start"
         };
         return(
             <div style={boxStyle}>
+                <Button icon={faEnvelope} name="phg5590@gmail.com"/>
                 <Button icon={faGithub} name="github.com/hwigyeompark"/>
             </div>
         );
