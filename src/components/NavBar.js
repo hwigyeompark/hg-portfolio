cac900286e9212024f88982d55dee601a142e710
hwigyeompark <phg5590@gmail.com>
2018-11-14 10:22:16 +0900

    update NavBar style to style.scss

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,40 +2,13 @@ import React, {Component} from 'react';
 
 class NavBar extends Component{
     render(){
-        const navStyle={
-            div : {
-                width:"100%",
-                height:"50px",
-                backgroundColor:"#ffffff",
-                opacity:".8",
-                position:"fixed",
-                boxShadow: "2px 2px #E0E0E0",
-                zIndex:"1"
-            },
-            ul:{
-                margin:"0 auto",
-                width:"60%",
-                height:"100%",
-                textAlign:"center"
-            },
-            li:{
-                width:"25%",
-                height:"100%",
-                float:"left",
-                textAlign:"center",
-                lineHeight:"47px",
-            },
-            a:{
-              color:"#333"
-            }
-        };
         return(
-          <div style={navStyle.div}>
-              <ul style={navStyle.ul}>
-                  <li style={navStyle.li}><a style={navStyle.a} href={process.env.PUBLIC_URL + "/#intro"}>HOME</a></li>
-                  <li style={navStyle.li}><a style={navStyle.a} href={process.env.PUBLIC_URL + "/#about"}>ABOUT</a></li>
-                  <li style={navStyle.li}><a style={navStyle.a} href={process.env.PUBLIC_URL + "/#projects"}>PROJECTS</a></li>
-                  <li style={navStyle.li}><a style={navStyle.a} href={process.env.PUBLIC_URL + "/#contact"}>CONTACT</a></li>
+          <div className="nav-bar-wrap">
+              <ul className="nav-bar">
+                  <li><a href={process.env.PUBLIC_URL + "/#intro"}>HOME</a></li>
+                  <li><a href={process.env.PUBLIC_URL + "/#about"}>ABOUT</a></li>
+                  <li><a href={process.env.PUBLIC_URL + "/#projects"}>PROJECTS</a></li>
+                  <li><a href={process.env.PUBLIC_URL + "/#contact"}>CONTACT</a></li>
               </ul>
           </div>
         );
