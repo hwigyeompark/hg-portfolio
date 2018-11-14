472dd99741e8db2d2d664f820db9ee13e89ef1cb
hwigyeompark <phg5590@gmail.com>
2018-11-14 12:23:09 +0900

    update MyRole style to style.scss

--- src/components/MyRole.js
+++ src/components/MyRole.js
@@ -3,29 +3,14 @@ import ThirdTitle from "./ThirdTitle";
 
 class MyRole extends Component{
     render(){
-        const style={
-            div:{
-                display:"flex",
-                flexDirection:"column",
-                marginLeft:"15%",
-                marginTop:"5%"
-            },
-            h1 : {
-                fontSize:"1.2rem"
-            },
-            p:{
-                wordBreak:"break-all",
-                fontSize:"1.1rem"
-            }
-        };
         return(
-            <div style={style.div}>
+            <div className="my-role">
                 <ThirdTitle thirdTitle="담당 역할"/>
-                <h1 style={style.h1}>{this.props.introTitle}</h1>
+                <h1>{this.props.introTitle}</h1>
                 <br/>
-                <p style={style.p}>{this.props.introMent1}</p>
-                <p style={style.p}>{this.props.introMent2}</p>
-                <p style={style.p}>{this.props.introMent3}</p>
+                <p>{this.props.introMent1}</p>
+                <p>{this.props.introMent2}</p>
+                <p>{this.props.introMent3}</p>
             </div>
         );
     }
