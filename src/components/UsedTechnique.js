ca7e17a835eb546592b2051c80058d3611d737e5
hwigyeompark <phg5590@gmail.com>
2018-11-14 12:18:13 +0900

    update UsedTechnique style to style.scss

--- src/components/UsedTechnique.js
+++ src/components/UsedTechnique.js
@@ -3,29 +3,14 @@ import ThirdTitle from "./ThirdTitle";
 
 class UsedTechnique extends Component{
     render(){
-        const style={
-            div:{
-                display:"flex",
-                flexDirection:"column",
-                marginLeft:"15%",
-                marginTop:"5%"
-            },
-            h1 : {
-                fontSize:"1.2rem"
-            },
-            p:{
-                wordBreak:"break-all",
-                fontSize:"1.1rem"
-            }
-        };
         return(
-            <div style={style.div}>
+            <div className="used-technique">
                 <ThirdTitle thirdTitle="사용 기술"/>
-                <h1 style={style.h1}>{this.props.introTitle1}</h1>
-                <p style={style.p}>{this.props.introMent1}</p>
+                <h1>{this.props.introTitle1}</h1>
+                <p>{this.props.introMent1}</p>
                 <br/>
-                <h1 style={style.h1}>{this.props.introTitle2}</h1>
-                <p style={style.p}>{this.props.introMent2}</p>
+                <h1>{this.props.introTitle2}</h1>
+                <p>{this.props.introMent2}</p>
             </div>
         );
     }
--- src/components/projects/HtmlHelper.js
+++ src/components/projects/HtmlHelper.js
@@ -34,10 +34,10 @@ class HtmlHelper extends Component{
                 introTitle2="Front-End" introMent2="Javascript, jQuery..."/>
                 <ScreenShot />
                     <ScreenShotBox pic={htmlHelper_2} explanation1="- Intro 페이지"/>
-                    <ScreenShotBox pic={htmlHelper_3} explanation1="- 작업 페이지" explanation2="- 좌측에 있는 HTML 태그를 가운데에 있는 드롭화면으로 Drag&Drop"/>
+                    <ScreenShotBox pic={htmlHelper_3} explanation1="- 작업 페이지" explanation2="- 좌측에 있는 HTML 태그를 가운데에 있는 드롭화면으로 드래그 앤 드롭"/>
                     <ScreenShotBox pic={htmlHelper_4} explanation1="- 태그를 선택하면 우측 속성창에서 태그 별 속성 및 스타일 편집 가능"/>
                     <ScreenShotBox pic={htmlHelper_1} explanation1="- 수정하고자 하는 속성 값과 스타일 값 입력 시, 실행 화면에서 적용"/>
-                    <ScreenShotBox pic={htmlHelper_5} explanation1="- 하단에는 Drag&Drop과 속성 및 스타일이 적용되고 있는 코드 반영"/>
+                    <ScreenShotBox pic={htmlHelper_5} explanation1="- 하단에는 드래그 앤 드롭으로 반영된 속성 및 스타일이 적용되고 있는지 코드로 확인 가능"/>
             </div>
         );
     }
