529a2eba080a5c8877f14add589c362301eb36ba
hwigyeompark <phg5590@gmail.com>
2018-12-12 18:19:01 +0900

    update ScreenShotItem border color

--- src/components/ScreenShotItem.js
+++ src/components/ScreenShotItem.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class ScreenShotItem extends Component{
     render() {
         return(
-                <div className="screen-shot-item" >
+                <div className="screen-shot-item">
                     <img src={this.props.pic}/>
                 </div>
         );
